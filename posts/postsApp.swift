d32fb019beb6b9a522fc314ab4a2235ad7ac6847
Jean <gofltmxj@naver.com>
2025-05-28 22:44:33 +0900

    Initial Commit

---
+++ posts/postsApp.swift
@@ -0,0 +1,17 @@
+//
+//  postsApp.swift
+//  posts
+//
+//  Created by Heawon Seo on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct postsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
